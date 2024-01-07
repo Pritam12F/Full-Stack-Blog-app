82d2f4e80de0124ae91f2bf6fe655ee7cc04bf76
Pritam Das <pritam.das.santu@gmail.com>
2024-01-07 17:12:59 +0530

    Update CreateBlogPost.jsx

--- frontend/src/components/CreateBlogPost.jsx
+++ frontend/src/components/CreateBlogPost.jsx
@@ -1,13 +1,44 @@
+import axios from "axios";
+import { useState } from "react";
+
 export function CreatePost() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
     <div>
-      <input type="text" placeholder="title"></input>
+      <input
+        type="text"
+        placeholder="title"
+        onChange={(ev) => {
+          setTitle(ev.target.value);
+        }}
+      ></input>
       <br></br>
       <br></br>
-      <input type="text" placeholder="description"></input>
+      <input
+        type="text"
+        placeholder="description"
+        onChange={(ev) => {
+          setDescription(ev.target.value);
+        }}
+      ></input>
       <br></br>
       <br></br>
-      <button>Create Post</button>
+      <button
+        onClick={() => {
+          axios
+            .post("http://localhost:3000/", {
+              title: title,
+              description: description,
+            })
+            .then((res) => {
+              alert(res.data);
+            });
+        }}
+      >
+        Create Post
+      </button>
     </div>
   );
 }
